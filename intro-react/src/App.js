2060c4077af1ca98cd74cdffe6c379c5fd71088c
OliviaDemaret <oliviadtqb@gmail.com>
2022-11-23 10:13:49 +0100

    Project Over, Add Local Storage UseEffect

--- intro-react/src/App.js
+++ intro-react/src/App.js
@@ -1,42 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
-import TodoList from './components/TodoList';
+import TodoList from "./components/TodoList";
 import AddTodo from "./components/AddTodo";
-import { useState } from "react";
-import {v4 as uuidv4 } from 'uuid';
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+let LSKEY = "Todo-App";
 
 function App() {
   let initialTodos = [
     {
       title: "First Todo",
       done: false,
-      id: uuidv4() 
+      id: uuidv4(),
     },
     {
       title: "Last Todo",
       done: true,
-      id: uuidv4()
-    }
-  ]
+      id: uuidv4(),
+    },
+  ];
 
   const [todos, setTodos] = useState(() => {
-    return initialTodos;
+    initialTodos = JSON.parse(localStorage.getItem(LSKEY + ".todos"));
+    return initialTodos || [];
   });
 
-  
-
   const addTodo = (todo) => {
-    setTodos([...todos, { id: uuidv4(), title: todo, done: false}]);
-  }
+    setTodos([...todos, { id: uuidv4(), title: todo, done: false }]);
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div className="App">
       <header className="App-header text-center flex flex-col">
         <h1 className="text-4xl font-bold my-5">My Todo App</h1>
         <hr></hr>
-          <AddTodo addTodo={addTodo}/>
+        <AddTodo addTodo={addTodo} />
         <hr></hr>
-          <TodoList todos={todos} setTodos={setTodos} />
+        <TodoList todos={todos} setTodos={setTodos} />
       </header>
     </div>
   );
--- intro-react/src/components/TodoList.js
+++ intro-react/src/components/TodoList.js
@@ -1,39 +1,39 @@
-const TodoList = ({todos, setTodos}) => {
-    const handleChange = (id) => {
-        const newTodo = todos.map((todo) => {
-          if (todo.id === id) {
-            todo.done = !todo.done;
-          }
-          return todo;
-        });
-    
-        setTodos(newTodo);
-      };
-    return(
-        <div>
-        <h2 className="m-5">Todos</h2>
-            <ul>
-                {todos.map(todo => (
-                    <li key={todo.id}>
-                        <input
-                            className="mx-2.5"
-                            type="checkbox"
-                            id={todo.id}
-                            name={todo.title}
-                            value={todo.title}
-                            checked={todo.done}
-                            onChange={() => {
-                                handleChange(todo.id);
-                              }}
-                        ></input>
-                        <label className="mx-2.5" htmlFor={todo.title}>
-                            {todo.title}
-                        </label>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+const TodoList = ({ todos, setTodos }) => {
+  const handleChange = (id) => {
+    const newTodo = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.done = !todo.done;
+      }
+      return todo;
+    });
 
-export default TodoList
+    setTodos(newTodo);
+  };
+  return (
+    <div>
+      <h2 className="m-5">Todos</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              className="mx-2.5"
+              type="checkbox"
+              id={todo.id}
+              name={todo.title}
+              value={todo.title}
+              checked={todo.done}
+              onChange={() => {
+                handleChange(todo.id);
+              }}
+            ></input>
+            <label className="mx-2.5" htmlFor={todo.title}>
+              {todo.title}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
